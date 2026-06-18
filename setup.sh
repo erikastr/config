d8079dad134a956fc5bc55f335c2ce000684a0bf
Erikas Truskauskas <erikas.truskauskas@gmail.com>
2026-06-18 19:09:35 +0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,9 +34,8 @@ done
 # kubectl
 [ -x "$HOME/.local/bin/kubectl" ] && echo "kubectl already installed, skipping." || {
   mkdir -p "$HOME/.local/bin"
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  chmod +x kubectl
-  mv ./kubectl "$HOME/.local/bin/kubectl"
+  curl -Lo "$HOME/.local/bin/kubectl" "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  chmod +x "$HOME/.local/bin/kubectl"
 }
 
 # .zshrc
